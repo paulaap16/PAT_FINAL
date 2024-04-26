433621b3bf6f943c11b9a0b7866aa0c91f1226ab
paulaap16 <202102628@alu.comillas.edu>
2024-04-26 16:22:06 +0200

    .

---
+++ pf/src/test/java/edu/comillas/icai/gitt/pat/spring/pf/PfApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.comillas.icai.gitt.pat.spring.pf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
