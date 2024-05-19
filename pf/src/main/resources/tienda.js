1b84df2d79538767c90541e025b37e8ec7869890
ClaraCPC <clara.palacios.spain@gmail.com>
2024-05-19 10:01:01 +0200

    clara1

---
+++ pf/src/main/resources/tienda.js
@@ -0,0 +1,40 @@
+function compruebaPass() {
+  let correcto = false;
+  correcto = document.getElementById("password").value ===
+             document.getElementById("passwordValidate").value;
+  if (correcto) mostrarAviso();
+  else mostrarAviso('✖︎ Contraseña inválida', 'error');
+  return correcto;
+}
+
+function registrarUsuario(datosJsonFormulario) {
+  if (!compruebaPass()) return;
+  fetch('/paulaphotography/user', {method: 'post', body: datosJsonFormulario, headers: {'content-type': 'application/json'}})
+    .then(response => {
+      if (response.ok) location.href = 'blog.html';
+      else if (response.status === 409) mostrarAviso('✖︎ Usuario ya registrado', 'error');
+      else mostrarAviso('✖︎ Error en el registro. Si ya tiene una cuenta, haga un Login', 'error');
+    });
+}
+
+function logUsuario(datosJsonFormulario) {
+  if (!compruebaPass()) return;
+  fetch('/paulaphotography/user/session', {method: 'post', body: datosJsonFormulario, headers: {'content-type': 'application/json'}})
+    .then(response => {
+      if (response.ok) location.href = 'blog.html';
+      else if (response.status === 409) mostrarAviso('✖︎ Usuario ya registrado', 'error');
+      else mostrarAviso('✖︎ Error en el login. Si no tiene cuenta, registrese primero', 'error');
+    });
+}
+
+function mostrarAviso(texto, tipo) {
+  const aviso = document.getElementById("aviso");
+  aviso.textContent = texto;
+  aviso.className = tipo;
+}
+
+function form2json(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  return JSON.stringify(Object.fromEntries(data.entries()));
+}
